b4d890d73a2eb1cf47158607739f4ead9ec210c7
Rodrigo Bondoc <rbondoc@Rodrigos-MacBook-Air.local>
2021-09-14 18:37:01 -0500

    added pattern attribute to Cells to invoke keypad on mobile

--- src/components/sudoku/SudokuCell.tsx
+++ src/components/sudoku/SudokuCell.tsx
@@ -125,6 +125,7 @@ export default function SudokuCell(props) {
                 }
                 min={0}
                 max={9}
+                pattern="[0-9]*"
                 value={grid[props.row][props.col]}          
                 className="sudoku-grid-cell-input"
                 disabled={(!props.isEditable || props.inHintGroup) || undefined}
